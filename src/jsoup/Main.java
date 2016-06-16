7c5109674b1c465915b6ffcd81edc5a0362b32f9
sim ratha <simrathahausongrupp@gmail.com>
2016-06-16 15:22:52 +0700

    scrapping

--- src/jsoup/Main.java
+++ src/jsoup/Main.java
@@ -1,7 +1,26 @@
 package jsoup;
 
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		String url ="https://www.khmerload.com/";
+		String hostname ="";
+		try{
+			
+			Document doc = Jsoup.connect(url).timeout(1000).get();
+			Elements element = doc.select("div.summary");
+			System.out.println(element.select("a"));
+		
+		}catch(Exception ex){
+			
+			System.out.println(ex.getMessage());
+		}
 	}
 }
